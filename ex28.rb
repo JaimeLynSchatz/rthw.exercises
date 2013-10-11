4e9effe76f579772c90f46773824acbf8d31d958
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-10-11 11:40:06 -0700

    changed output to display evals as more as asserts

--- ex28.rb
+++ ex28.rb
@@ -1,25 +1,22 @@
-truth_test = 
-	(true and true) == true
-	(false and true) == false
-	(1 == 1 and 2 == 1) == false
-	("test" == "test") == true
-	((1 == 1) or (2 != 0)) == true
-	(true and (1 == 1)) == true
-	(false and (0 != 0)) == false
-	(true or (1 == 1)) == true
-	("test" == "testing") == false
-	((1 != 0) and (2 == 1)) == false
-	("test" != "testing") == true
-	("test" == 1) == false
-	(not (true and false)) == true
-	(not ((1 == 1) and (0 != 1))) == false
-	(not ((10 == 1) or (1000 == 1000))) == false
-	(not ((1 != 10) or (3 == 4))) == false
-	(not (("testing" == "testing") and ("Zed" == "Cool Guy"))) == true
-	((1 == 1) and not ("testing" == 1 or 1 == 0 )) == true
-	(("chunky" == "bacon") and not ((3 == 4) or (3 == 3))) == false
-	((3 == 3) and not ("testing" == "testing" or "Ruby" == "Fun")) == false
-
-puts
-puts truth_test
+puts "Should all say 'true'"
+puts (true and true) == true
+puts (false and true) == false
+puts (1 == 1 and 2 == 1) == false
+puts ("test" == "test") == true
+puts ((1 == 1) or (2 != 0)) == true
+puts (true and (1 == 1)) == true
+puts (false and (0 != 0)) == false
+puts (true or (1 == 1)) == true
+puts ("test" == "testing") == false
+puts ((1 != 0) and (2 == 1)) == false
+puts ("test" != "testing") == true
+puts ("test" == 1) == false
+puts (not (true and false)) == true
+puts (not ((1 == 1) and (0 != 1))) == false
+puts (not ((10 == 1) or (1000 == 1000))) == false
+puts (not ((1 != 10) or (3 == 4))) == false
+puts (not (("testing" == "testing") and ("Zed" == "Cool Guy"))) == true
+puts ((1 == 1) and not ("testing" == 1 or 1 == 0 )) == true
+puts (("chunky" == "bacon") and not ((3 == 4) or (3 == 3))) == false
+puts ((3 == 3) and not ("testing" == "testing" or "Ruby" == "Fun")) == false
 puts
