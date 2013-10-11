10e836e93d82d5197cae5a77cf3d30cb96ad2585
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-10-11 13:37:11 -0700

    newly completed exercise 34

---
+++ ex34.rb
@@ -0,0 +1,14 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts animals
+
+print "\nThe animal at index 1 is the python? ", animals[1] == "python"
+print "\nThe 3rd animal is at index 2, the peacock? ", animals[2] == "peacock"
+print "\nThe first animal is at index 0, the bear? ", animals[0] == "bear"
+print "\nThe animal at index 3 is the kangaroo? ", animals[3] == "kangaroo"
+print "\nThe 5th animal is at index 4, the whale? ", animals[4] == "whale"
+print "\nThe animal at index 2 is the peacock? ", animals[2] == "peacock"
+print "\nThe 6th animal is at index 5, the platypus? ", animals[5] == "platypus"
+print "\nThe animal at index 4 is the whale? ", animals[4] == "whale"
+
+puts 
