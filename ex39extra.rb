2b58b69794e7eda68000c7bf3413e685d6cba5fc
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2013-10-13 20:13:50 -0700

    newly completed exercise 39

---
+++ ex39extra.rb
@@ -0,0 +1,14 @@
+da_fam = {
+	'dad' => 'Rob', 
+	'mom' => 'Jaime', 
+	'son' => 'Danny', 
+	'youngest' => 'Chloe', 
+	'middle' => 'Madeleine'
+	}
+
+puts da_fam
+	
+da_fam['dog'] = 'Timo'
+da_fam['cat'] = 'Smokey'
+
+puts da_fam
