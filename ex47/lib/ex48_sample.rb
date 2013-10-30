3fb8ec35429c06ac70acc3e4d29b6b9c32684cb6
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-10-30 04:32:25 -0700

    create ex48_sample.rb

---
+++ ex47/lib/ex48_sample.rb
@@ -0,0 +1,8 @@
+stuff = gets.chomp()
+words = stuff.split()
+
+Pair = Struct.new(:token, :word)
+first_word = Pairn.new("direction", "north")
+second_word = Pair.new("verb", "go")
+sentence = [first_word, second_word]
+
