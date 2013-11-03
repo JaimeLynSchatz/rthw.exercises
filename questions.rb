11e3cab760db790b5f8cae8f18f3b17362609149
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2013-11-02 18:47:52 -0700

    create questions.rb

---
+++ questions.rb
@@ -0,0 +1,10 @@
+# computer questions
+
+questions = [
+"0. Why do computers always start counting at 0?", 
+"1. If you had 2 computer monitors which one would you use?the wider one or the skinnier one?",
+"2. do you want jelly or jam?",
+"3. What kind of questions do you like?"
+]
+
+puts questions[0]
